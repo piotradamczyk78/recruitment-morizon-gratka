2ed3d035d79e66ebc31dd815640e4d8bbc9cf7c9
Piotr Adamczyk <piotr.k.adamczyk@gmail.com>
2026-04-10 03:56:10 +0200

    fix(likes): eliminacja N+1 query w galerii
    Zamiana petli z osobnym zapytaniem hasUserLikedPhoto() per zdjecie
    na jedno zapytanie getUserLikedPhotoIds() pobierajace wszystkie
    lajki usera jednym SELECT z IN clause.
    
    Zmiany:
    - LikeRepositoryInterface: nowa metoda getUserLikedPhotoIds()
    - LikeRepository: implementacja z jednym zapytaniem
    - HomeController: jedno wywolanie zamiast petli N+1
    
    Fixes #10

--- symfony-app/src/Likes/LikeRepositoryInterface.php
+++ symfony-app/src/Likes/LikeRepositoryInterface.php
@@ -14,4 +14,10 @@ interface LikeRepositoryInterface
     public function createLike(Photo $photo): Like;
 
     public function updatePhotoCounter(Photo $photo, int $increment): void;
+
+    /**
+     * @param Photo[] $photos
+     * @return array<int, bool> Map of photo ID => liked status
+     */
+    public function getUserLikedPhotoIds(array $photos): array;
 }
