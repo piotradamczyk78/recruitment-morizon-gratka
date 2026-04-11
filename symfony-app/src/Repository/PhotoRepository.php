15129528fa26a947a3e1026e4854384279140d90
Piotr Adamczyk <piotr.k.adamczyk@gmail.com>
2026-04-11 14:05:09 +0200

    feat: dodaj metode findByFilters() w PhotoRepository
    Dynamiczne budowanie QueryBuilder na podstawie filtrow:
    - location, camera, description - LIKE (czesciowe dopasowanie)
    - username - LIKE na u.username (JOIN)
    - taken_at - zakres dat (date_from / date_to)
    Gdy brak filtrow - zwraca wszystkie zdjecia z eager-loaded userami.
    
    Closes #56

--- symfony-app/src/Repository/PhotoRepository.php
+++ symfony-app/src/Repository/PhotoRepository.php
@@ -24,4 +24,44 @@ class PhotoRepository extends ServiceEntityRepository
             ->getQuery()
             ->getResult();
     }
+
+    public function findByFilters(array $filters): array
+    {
+        $qb = $this->createQueryBuilder('p')
+            ->leftJoin('p.user', 'u')
+            ->addSelect('u')
+            ->orderBy('p.id', 'ASC');
+
+        if (!empty($filters['location'])) {
+            $qb->andWhere('p.location LIKE :location')
+                ->setParameter('location', '%' . $filters['location'] . '%');
+        }
+
+        if (!empty($filters['camera'])) {
+            $qb->andWhere('p.camera LIKE :camera')
+                ->setParameter('camera', '%' . $filters['camera'] . '%');
+        }
+
+        if (!empty($filters['description'])) {
+            $qb->andWhere('p.description LIKE :description')
+                ->setParameter('description', '%' . $filters['description'] . '%');
+        }
+
+        if (!empty($filters['username'])) {
+            $qb->andWhere('u.username LIKE :username')
+                ->setParameter('username', '%' . $filters['username'] . '%');
+        }
+
+        if (!empty($filters['date_from'])) {
+            $qb->andWhere('p.takenAt >= :date_from')
+                ->setParameter('date_from', new \DateTimeImmutable($filters['date_from']));
+        }
+
+        if (!empty($filters['date_to'])) {
+            $qb->andWhere('p.takenAt <= :date_to')
+                ->setParameter('date_to', new \DateTimeImmutable($filters['date_to'] . ' 23:59:59'));
+        }
+
+        return $qb->getQuery()->getResult();
+    }
 }
